ef9e53dafc2b71f2ae142d283aab192bc20edcda
beqaperanidze <beqaperanidze28@gmail.com>
2025-04-13 12:45:26 +0400

    initial commit

---
+++ src/main/java/com/studybuddyai/StudyBuddyAiApplication.java
@@ -0,0 +1,13 @@
+package com.studybuddyai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBuddyAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyBuddyAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/studybuddyai/StudyBuddyAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studybuddyai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBuddyAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
