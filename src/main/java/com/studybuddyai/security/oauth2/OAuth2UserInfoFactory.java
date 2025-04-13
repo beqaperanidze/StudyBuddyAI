6b76d14fd9e4b135308db7c824f26ef4875831e2
beqaperanidze <beqaperanidze28@gmail.com>
2025-04-13 16:01:30 +0400

    kept only google oauth2. might add jwt later

--- src/main/java/com/studybuddyai/security/oauth2/OAuth2UserInfoFactory.java
+++ src/main/java/com/studybuddyai/security/oauth2/OAuth2UserInfoFactory.java
@@ -8,8 +8,6 @@ public class OAuth2UserInfoFactory {
     public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) throws OAuth2AuthenticationProcessingException {
         if(registrationId.equalsIgnoreCase("google")) {
             return new GoogleOAuth2UserInfo(attributes);
-        } else if (registrationId.equalsIgnoreCase("github")) {
-            return new GithubOAuth2UserInfo(attributes);
         } else {
             throw new OAuth2AuthenticationProcessingException("Login with " + registrationId + " is not supported yet.");
         }
